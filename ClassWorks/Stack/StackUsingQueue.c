5cb6808ef6c0326ced08b0d6cea3e48dbafed046
asmitsaha5 <asmitsaha5@gmail.com>
2024-09-17 23:59:54 +0530

    ....

--- ClassWorks/Stack/StackUsingQueue.c
+++ ClassWorks/Stack/StackUsingQueue.c
@@ -1,8 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     int n;
-    printf("Enter the number of elements to store :");
+    printf("Enter length of the stack ");
     scanf("%d", &n);
+    int q1[n];
+    int q2[n];
+    int front1 = -1;
+    int front2 = -1;
+    int rear1 = -1;
+    int rear2 = -1;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        printf("Enter the element %d", i + 1);
+        scanf("%d", &x);
+        if (rear2 == n - 1)
+        {
+            printf("overflow");
+        }
+        else if (rear2 == -1 && front2 == -1)
+        {
+            rear2 = rear2 + 1;
+            front2 = front2 + 1;
+        }
+        else
+        {
+            rear2 = rear2 + 1;
+        }
+        printf("%d", q)
+    }
+    return 0;
 }
