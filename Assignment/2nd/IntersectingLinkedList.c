4211b754c9fa8766f4562093c9ec295244d64392
asmitsaha5 <asmitsaha5@gmail.com>
2024-10-04 16:23:57 +0530

    ...

--- Assignment/2nd/IntersectingLinkedList.c
+++ Assignment/2nd/IntersectingLinkedList.c
@@ -7,58 +7,110 @@ struct Node
     struct Node *next;
 };
 
-struct Node *intersectionnode(struct Node *head1, struct Node *head2)
+struct Node *newNode(int data)
 {
-    struct Node *temp;
-    while (head2 != NULL)
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+void push(struct Node *stack[], int *top, struct Node *node)
+{
+    stack[++(*top)] = node;
+}
+
+struct Node *pop(struct Node *stack[], int *top)
+{
+    return stack[(*top)--];
+}
+
+struct Node *findIntersection(struct Node *head1, struct Node *head2)
+{
+    struct Node *stack1[100], *stack2[100];
+    int top1 = -1, top2 = -1;
+
+    struct Node *temp = head1;
+    while (temp)
     {
-        temp = head1;
-        while (temp != NULL)
+        push(stack1, &top1, temp);
+        temp = temp->next;
+    }
+
+    temp = head2;
+    while (temp)
+    {
+        push(stack2, &top2, temp);
+        temp = temp->next;
+    }
+
+    struct Node *intersection = NULL;
+    while (top1 >= 0 && top2 >= 0)
+    {
+        if (stack1[top1] == stack2[top2])
         {
-            if (temp == head2)
-                return head2;
-            temp = temp->next;
+            intersection = pop(stack1, &top1);
+            pop(stack2, &top2);
         }
-        head2 = head2->next;
+        else
+        {
+            break;
+        }
     }
-    return NULL;
+
+    return intersection;
 }
 
-void printList(struct Node *node)
+void printList(struct Node *head)
 {
-    while (node != NULL)
+    while (head)
     {
-        printf(" %d", node->data);
-        node = node->next;
+        printf("%d -> ", head->data);
+        head = head->next;
     }
-    printf("\n");
-}
-
-struct Node *createNode(int new_data)
-{
-    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
-    new_node->data = new_data;
-    new_node->next = NULL;
-    return new_node;
+    printf("NULL\n");
 }
 
 int main()
 {
-    struct Node *head1 = createNode(10);
-    head1->next = createNode(15);
-    head1->next->next = createNode(30);
+    struct Node *newNode1 = newNode(50);
+    struct Node *newNode2 = newNode(40);
+    struct Node *newNode3 = newNode(30);
+    struct Node *newNode4 = newNode(15);
+    struct Node *newNode5 = newNode(10);
 
-    struct Node *head2 = createNode(3);
-    head2->next = createNode(6);
-    head2->next->next = createNode(9);
-    head2->next->next->next = head1->next;
+    newNode5->next = newNode4;
+    newNode4->next = newNode3;
+    newNode3->next = newNode2;
+    newNode2->next = newNode1;
 
-    struct Node *intersectionPoint = intersectionnode(head1, head2);
+    struct Node *head1 = newNode5;
 
-    if (intersectionPoint == NULL)
-        printf("No Intersection Point\n");
+    struct Node *newNode6 = newNode(9);
+    struct Node *newNode7 = newNode(6);
+    struct Node *newNode8 = newNode(3);
+
+    newNode8->next = newNode7;
+    newNode7->next = newNode6;
+    newNode6->next = newNode3;
+
+    struct Node *head2 = newNode8;
+
+    printf("List 1: ");
+    printList(head1);
+    printf("List 2: ");
+    printList(head2);
+
+    struct Node *intersection = findIntersection(head1, head2);
+
+    if (intersection)
+    {
+        printf("The intersection point is: %d\n", intersection->data);
+    }
     else
-        printf("Intersection Point: %d\n", intersectionPoint->data);
+    {
+        printf("No intersection point found.\n");
+    }
 
     return 0;
 }
